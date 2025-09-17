59068f480777c941a69108887ff5c43cd849baa8
HectorPetersen <hector.guldager.petersen@gmail.com>
2025-09-17 09:35:56 +0200

    Book

--- src/LoanShop/Book.java
+++ src/LoanShop/Book.java
@@ -1,4 +1,28 @@
 package LoanShop;
+// Subklasse
+public class Book extends Items {
 
-public class Book {
+    public Book(String title, String type) { super(title, type); }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String setTitle(){
+        return title;
+    }
+
+    public String setType(){
+        return type;
+    }
+
+    // toString metode
+    @Override
+    public String toString() {
+        return "Registered: " + type + " - " + title;
+    }
 }
