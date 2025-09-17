35944752633c2322b141b2e68951e2ba1beea98e
Zander3-2 <zaen0001@stud.ek.dk>
2025-09-17 09:26:52 +0200

    Konstruktør, Getters og Setters :)

--- src/LoanShop/Items.java
+++ src/LoanShop/Items.java
@@ -1,4 +1,21 @@
 package LoanShop;
 
 public class Items {
+    protected String name;
+    protected String type;
+
+    //Laver en Constructor
+    Items(String name, String type ) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+            return type;
+    }
+public void SetName(String name){this.name = name;}
+public void SetType(String type){this.type = type;}
 }
