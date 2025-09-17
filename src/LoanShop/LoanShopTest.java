11e6017e0eee7e7764a5ef57d141fa2ddb70e3d0
MagnusNH <Magn763j@stud.ek.dk>
2025-09-17 09:59:58 +0200

    Loan Shop af Zander, Hector og Magnus

--- src/LoanShop/LoanShopTest.java
+++ src/LoanShop/LoanShopTest.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class LoanShopTest {
     public static void main (String [] args){
         welcomeMessage();
+    }
+
+    public static void welcomeMessage(){
+        System.out.println("**********************************");
+        System.out.println("**\uD83D\uDCD6Welcome to the loan shop!\uD83D\uDCD6**");
+        System.out.println("**\uD83D\uDCFD We have Books and Videos!\uD83D\uDCFD**");
+        System.out.println("**********************************");
+    }
+
+    public static void userInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many items would you like to loan? ");
+        int i = scanner.nextInt();
+
 
     }
 }
